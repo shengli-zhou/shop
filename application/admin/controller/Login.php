72f655e6b379db09d8fa782ff1b9e3c687747732
zhoushengli <1664713624@qq.com>
2019-06-27 09:16:37 +0800

    第二次上传

---
+++ application/admin/controller/Login.php
@@ -0,0 +1,44 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+use think\captcha\Captcha;
+use Request;
+use Db;
+use think\facade\Session;
+use gmars\rbac\Rbac;
+class Login extends Controller
+{
+
+    public function login()
+    {
+       return $this->fetch();
+    }
+    public function loginAction()
+    {
+    	$code=Request::post('code');
+    	$name=Request::post('name');
+    	$password=Request::post('password');
+    	$captcha = new Captcha();
+    	if( !$captcha->check($code)){
+    		$arr=['code'=>'1','status'=>'error','message'=>'验证码错误'];
+    	}else{
+    		$where=['name'=>$name,'password'=>md5($password)];
+    		$res=Db::table('admin')->where($where)->find();
+    		if (empty($res)){
+    			$arr=['code'=>'2','status'=>'error','message'=>"账号或者密码错误"];
+    		}else{
+    			$arr=['code'=>'0','status'=>'ok','message'=>"登陆成功"];
+    			Session::set("name",$name);
+                $rbac = new Rbac();
+                $rbac->cachePermission($res['id']);
+    		}
+    	}
+    	$json=json_encode($arr);
+    	echo $json;
+    }
+    public function loginOut(){
+    	Session::delete('name');
+    	$this->redirect('login/login');
+    }
+
+}
